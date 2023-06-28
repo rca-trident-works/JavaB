9108348739c3163b2a74172d38a6e797aebbd8f4
ろむねこ <66072112+r-ca@users.noreply.github.com>
2023-06-28 15:04:38 +0900

    2_1_1, 2_1_2

---
+++ src/Practice2_1_1.java
@@ -0,0 +1,8 @@
+public class Practice2_1_1 {
+    public static void main(String[] args) {
+        int int1 = 2;
+        int int2 = 0;
+        int int3 = int1 / int2;
+        System.out.println("int1 / int2 = " + int3);
+    }
+}
---
+++ src/Practice2_1_2.java
@@ -0,0 +1,15 @@
+public class Practice2_1_2 {
+    public static void main(String[] args) {
+        int int1 = 2;
+        int int2 = 0;
+        try {
+            int int3 = int1 / int2;
+            System.out.println("int1 / int2 = " + int3);
+        }
+            catch (ArithmeticException e) {
+            System.out.println("0による割り算がされました。");
+            System.out.println(e);
+        }
+        System.out.println("プログラムを終了します。");
+    }
+}
